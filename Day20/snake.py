e1c381d006b197cf5b0490c4ba82d484cf218135
Nathan Cashman <nathan.t.cashman@gmail.com>
2022-10-09 10:58:10 +0100

    adding existing work

---
+++ Day20/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.snake_body = []
+        self.create_snake()
+        self.head = self.snake_body[0]
+
+    def create_snake(self):
+        for position in POSITIONS:
+            snake_part = Turtle(shape="square")
+            snake_part.penup()
+            snake_part.color("white")
+            snake_part.goto(position)
+            self.snake_body.append(snake_part)
+
+    def move(self):
+        for part in range(len(self.snake_body) - 1, 0, -1):
+            new_x = self.snake_body[part - 1].xcor()
+            new_y = self.snake_body[part - 1].ycor()
+            self.snake_body[part].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
